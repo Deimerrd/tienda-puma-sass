d3b684f23669d4c3c562c5514c25bbe3fb5990d3
Deimerd <deimerblandonmc@gmail.com>
2026-07-27 23:02:44 -0500

    Refactor: Layout y Settings separados en componentes

--- src/Componentes/AdminView.jsx
+++ src/Componentes/AdminView.jsx
@@ -2,6 +2,7 @@ import { useState, useMemo } from "react";
 import jsPDF from "jspdf";
 import AdminLayout from "./admin/layout/AdminLayout";
 import SecurityManager from "./admin/settings/SecurityManager";
+import PaymentsManager from "./admin/payments/PaymentsManager";
 import {
   BarChart,
   Bar,
@@ -506,55 +507,12 @@ function AdminView({
         setNuevaContrasena={setNuevaContrasena}
         cambiarClave={cambiarClave}
       />
-      <div
-        style={{
-          background: "#e0f2fe",
-          padding: "15px",
-          borderRadius: "8px",
-          marginBottom: "25px",
-        }}
-      >
-        <h3>💳 Configuración Nequi</h3>
-
-        <div style={{ marginBottom: "10px" }}>
-          <label>Número Nequi:</label>
-
-          <input
-            type="text"
-            value={nequiNumero}
-            onChange={(e) => setNequiNumero(e.target.value)}
-            placeholder="3001234567"
-            style={{ marginLeft: "10px" }}
-          />
-        </div>
-
-        <div>
-          <label
-            style={{
-              fontWeight: "700",
-              display: "block",
-              marginTop: "20px",
-              marginBottom: "10px",
-            }}
-          >
-            🖼 Código QR
-          </label>
-
-          <input type="file" accept="image/*" onChange={subirQR} />
-          {nequiQR && (
-            <img
-              src={nequiQR}
-              alt="QR Nequi"
-              style={{
-                width: "180px",
-                marginTop: "15px",
-                borderRadius: "12px",
-                border: "1px solid #ddd",
-              }}
-            />
-          )}
-        </div>
-      </div>
+      <PaymentsManager
+        nequiNumero={nequiNumero}
+        setNequiNumero={setNequiNumero}
+        subirQR={subirQR}
+        nequiQR={nequiQR}
+      />
 
       {/* 🛠️ 2. CREADOR DE CATEGORÍAS DINÁMICAS */}
       <div
