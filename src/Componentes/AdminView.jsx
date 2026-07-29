750cc568d5f7ee0fd1c7dd72dff8c712fd93048b
Deimerd <deimerblandonmc@gmail.com>
2026-07-29 10:10:11 -0500

    dividi tre componente del adminView

--- src/Componentes/AdminView.jsx
+++ src/Componentes/AdminView.jsx
@@ -3,6 +3,7 @@ import jsPDF from "jspdf";
 import AdminLayout from "./admin/layout/AdminLayout";
 import SecurityManager from "./admin/settings/SecurityManager";
 import PaymentsManager from "./admin/payments/PaymentsManager";
+import CategoryManager from "./admin/categories/CategoryManager";
 import {
   BarChart,
   Bar,
@@ -513,67 +514,13 @@ function AdminView({
         subirQR={subirQR}
         nequiQR={nequiQR}
       />
-
-      {/* 🛠️ 2. CREADOR DE CATEGORÍAS DINÁMICAS */}
-      <div
-        style={{
-          background: "#fef08a",
-          padding: "15px",
-          border: "1px solid #eab308",
-          borderRadius: "8px",
-          marginBottom: "25px",
-        }}
-      >
-        <h3 style={{ margin: "0 0 10px 0", color: "#854d0e" }}>
-          🛠️ Creador de Categorías Dinámicas
-        </h3>
-        <label htmlFor="txt-nueva-cat">Nombre de la nueva categoría: </label>
-        <input
-          id="txt-nueva-cat"
-          type="text"
-          placeholder="Ej: Gorras"
-          value={nuevaCatNombre}
-          onChange={(e) => setNuevaCatNombre(e.target.value)}
-        />
-        <label
-          style={{
-            display: "block",
-            marginTop: "12px",
-            marginBottom: "5px",
-            fontWeight: "700",
-          }}
-        >
-          Icono de la categoría
-        </label>
-
-        <select
-          value={iconoCategoria}
-          onChange={(e) => setIconoCategoria(e.target.value)}
-          style={{
-            padding: "8px",
-            borderRadius: "8px",
-            width: "220px",
-          }}
-        >
-          <option value="👕">👕 Camisas</option>
-          <option value="👟">👟 Zapatos</option>
-          <option value="👖">👖 Pantalones</option>
-          <option value="🧥">🧥 Chaquetas</option>
-          <option value="🧶">🧶 Suéteres</option>
-          <option value="⌚">⌚ Accesorios</option>
-        </select>
-
-        <button
-          onClick={() => {
-            agregarCategoria(nuevaCatNombre, iconoCategoria);
-            setNuevaCatNombre("");
-            setIconoCategoria("👕");
-          }}
-          style={{ marginLeft: "10px", cursor: "pointer" }}
-        >
-          ➕ Crear Categoría
-        </button>
-      </div>
+      <CategoryManager
+        nuevaCatNombre={nuevaCatNombre}
+        setNuevaCatNombre={setNuevaCatNombre}
+        iconoCategoria={iconoCategoria}
+        setIconoCategoria={setIconoCategoria}
+        agregarCategoria={agregarCategoria}
+      />
 
       {/* 📊 3. SECCIÓN GLOBALES (El menú que despliega todo según lo que elijas) */}
       <div
